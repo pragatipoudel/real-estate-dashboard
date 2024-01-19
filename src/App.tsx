87fab1866af0307e11c83b3e1e61f2b25830a2a8
Pragati Poudel <pragati.poudel37@gmail.com>
2024-01-19 15:33:24 -0500

    Use Appshell in Mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import './App.css';
+import '@mantine/core/styles.css';
+import { AppShell, MantineProvider, Title } from '@mantine/core';
+import { LineChart } from '@mantine/charts';
+import { data } from './data';
 
 function App() {
-    return <div>You are at dashboard.</div>;
+    return (
+        <MantineProvider>
+            <AppShell header={{ height: 60 }} padding="md">
+                <AppShell.Header>
+                    <Title px="md">Real Estate Dashboard</Title>
+                </AppShell.Header>
+
+                <AppShell.Main>Main</AppShell.Main>
+            </AppShell>
+        </MantineProvider>
+    );
 }
 
 export default App;
